1bfa3b3486b528bb49be33743410c6a95d1502b1
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-08-16 14:25:20 -0400

    Externalizing input variables `SOURCE_DIR` and `COMMIT_MESSAGE`  (#1)
    * externalize SOURCE_DIR
    
    * reorder the input vars
    
    * enable push in target repo
    
    * updated command
    
    * updated command
    
    * externalizing commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@ set -e
 set -x
 
 echo "Reading input variables"
-DESTINATION_REPOS_BRANCHES="$1"
+SOURCE_DIR="$1"
+DESTINATION_REPOS_BRANCHES="$2"
+COMMIT_MESSAGE="$3"
 WORK_DIR=`pwd`
 
 git config --global user.email "$GIT_USEREMAIL"
 git config --global user.name "$GIT_USERNAME"
+
+printf "\n\n $SOURCE_DIR \n $DESTINATION_REPOS_BRANCHES \n $COMMIT_MESSAGE \n\n"
 
 for repo in $DESTINATION_REPOS_BRANCHES
 do
@@ -22,17 +26,16 @@ do
 
   echo "Cloning destination git repository: $repo"
   git clone --single-branch --branch ${arrRepo[1]} "https://x-access-token:$API_TOKEN_GITHUB@${arrRepo[0]}" "$CLONE_DIR"
-  ls -al $CLONE_DIR
   ls -al $CLONE_DIR/
-  cp -R "k8s/pkg" "$CLONE_DIR/"
+  cp -a "$SOURCE_DIR/." "$CLONE_DIR/"
   cd "$CLONE_DIR"
   git add .
 
   if git status | grep -q "Changes to be committed"
   then
     git status
-    git commit -m "checking files"
-#    git push origin ${arrIN[1]}
+    git commit -m "$COMMIT_MESSAGE"
+    git push origin ${arrIN[1]}
   else
     echo "No changes detected"
   fi
